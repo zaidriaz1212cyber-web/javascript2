68c7adea0ec4f3671b1de9cbf24b47694130049c
zaid <zaidriaz1212cyber@gmail.com>
2025-11-23 21:39:13 +0500

    javascript2

---
+++ app.js
@@ -0,0 +1,50 @@
+var username;
+var myname1 = " my name is : zaid bin riaz";
+var age = "12 year old";
+var course = " course: Web and app development- batch:18";
+var school = " school: Smit"
+
+alert(myname1);
+alert(age);
+alert(course);
+alert(school)
+
+
+var piz1 = "p";
+var piz2 = "pi";
+var piz3 = "piz";
+var piz4 = "pizz";
+var piz5 = "pizza"
+
+alert(piz1);
+alert(piz2);
+alert(piz3);
+alert(piz4);
+alert(piz5)
+
+
+var email = " my email is : zaidriaz1212cyber@gmail.com"
+
+alert(email)
+
+
+
+var book1 = " book : A smarter way to learn javascript"
+
+message = alert(book1)
+
+
+var exercise4 = "name : zaidbinriaz";
+var age = "12 year old";
+var study = "study in SMIT";
+var course = "currently doing web and application development-batch: 18"
+
+alert(exercise4);
+alert(age);
+alert(study);
+alert(course)
+
+var hello = "Yay! i can write html code through javascript";
+ 
+
+document.write(hello);
